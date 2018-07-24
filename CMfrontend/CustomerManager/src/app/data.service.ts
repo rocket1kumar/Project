76c0cfa2a6b7f616f9bf5ae639708c8fa2451144
rocket1kumar <rocket1kumar.live@gmail.com>
2018-07-24 14:44:45 +0530

    database host set

--- CMfrontend/CustomerManager/src/app/data.service.ts
+++ CMfrontend/CustomerManager/src/app/data.service.ts
@@ -16,20 +16,20 @@ export class DataService {
 // }
 
 saveCustomer(customer){
-	return this.http.post("http://localhost:8080/api/savecustomer",customer);//.map(res => { return res;});
+	return this.http.post("http://192.168.10.156:8080/api/savecustomer",customer);//.map(res => { return res;});
 }
 
 getData(){
-	return this.http.get("http://localhost:8080/api/customers");
+	return this.http.get("http://192.168.10.156:8080/api/customers");
 
 }
 
 deleteCustomerById(id){
-	 return this.http.delete("http://localhost:8080/api/delete/"+id);
+	 return this.http.delete("http://192.168.10.156:8080/api/delete/"+id);
 }
 
 getDataById(id){
-	return this.http.get("http://localhost:8080/api/customer/"+id);
+	return this.http.get("http://192.168.10.156:8080/api/customer/"+id);
 }
 
 }
